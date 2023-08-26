7cb9b1bbc040fcd43ee3e5b8434807703cccc9d2
Raghav Kaushik <raghav.kaushik611@gmail.com>
2023-08-26 19:33:58 +0530

    added PWA integeration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = { reactStrictMode: true }
+const withPWA = require( 'next-pwa' )
 
+module.exports = withPWA( {
+  pwa: {
+    dest: 'public',
+    disable: 'development' === process.env.NODE_ENV,
+    register: true,
+    skipWaiting: true,
+  },
+} )
 module.exports = nextConfig
