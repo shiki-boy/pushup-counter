6bed46261630b926e0e5e3e293c2a0acc4e6af94
Raghav Kaushik <raghav.kaushik611@gmail.com>
2023-08-26 17:45:46 +0530

    added mongo

---
+++ src/pages/api/add-pushup.js
@@ -0,0 +1,37 @@
+import { format } from 'date-fns'
+
+import clientPromise from '@/lib/mongoConnect'
+
+export default async function handler( req, res ) {
+  try {
+    const client = await clientPromise
+    const db = client.db( 'exercise' )
+    const exerciseColl = db.collection( 'pushups' )
+
+    const today = format( new Date(), 'dd/MM/yyyy' )
+    // insert pushups by day
+    // find pushup for the day
+    const doc = await exerciseColl.findOne( { date: today } )
+
+    if ( doc ) {
+      const result = await exerciseColl.updateOne(
+        { _id: doc._id },
+        { $set: { count: doc.count + 1 } },
+      )
+
+      return res.status( 200 ).json( result )
+    }
+
+    // if not found create one
+    const result = await exerciseColl.insertOne(
+      {
+        count: 1,
+        date: today,
+      },
+    )
+
+    return res.status( 200 ).json( result )
+  } catch ( e ) {
+    console.error( e )
+  }
+}
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from '@/lib/mongoConnect'
+import { format } from 'date-fns'
 
-export default function handler( req, res ) {
-  res.status( 200 ).json( { name: 'John Doe' } )
+export default async function handler( req, res ) {
+  try {
+    const client = await clientPromise
+    const db = client.db( 'exercise' )
+
+    const exerciseColl = db.collection( 'pushups' )
+
+    const today = format( new Date(), 'dd/MM/yyyy' )
+
+    const result = await exerciseColl.insertOne(
+      {
+        count: 1,
+        date: today,
+      },
+    )
+
+
+    // const result = await db
+    //   .collection( 'pushups' )
+    //   .find( {} )
+    //   // .sort( { metacritic: -1 } )
+    //   .limit( 10 )
+    //   .toArray()
+
+    res.status( 200 ).json( result )
+  } catch ( e ) {
+    console.error( e )
+  }
 }
